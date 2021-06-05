d131c21c94a9b00bfc8bb756fa89c68d5293d954
MaksymilianJodłowski <10886532+ravensiris@users.noreply.github.com>
2021-06-05 09:05:32 +0000

    "gh-pages"

---
+++ precache-manifest.3c5fa00f9f8948a4cbd8b20b5593aaa0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3673cde36de1467d4c9",
+    "url": "/librelibrus/css/app.2fff93e2.css"
+  },
+  {
+    "revision": "9801edc13feed3f43f3e",
+    "url": "/librelibrus/css/chunk-339dc444.f234e47d.css"
+  },
+  {
+    "revision": "bbaf2a10486f37bdbb55",
+    "url": "/librelibrus/css/chunk-3f6f6742.942ef5ed.css"
+  },
+  {
+    "revision": "093782eaa1fd8a94773f",
+    "url": "/librelibrus/css/chunk-5ff8d886.93b0b5e8.css"
+  },
+  {
+    "revision": "8fe0d42320c18b433e09",
+    "url": "/librelibrus/css/chunk-9d33c6c4.ffe15972.css"
+  },
+  {
+    "revision": "a3e0b5f427803a187c1b62c5919196aa",
+    "url": "/librelibrus/fonts/Poppins-Bold.a3e0b5f4.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/librelibrus/fonts/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "d35ae01f7f8aa44d47f7eae44b95323d",
+    "url": "/librelibrus/img/calendar4-week.d35ae01f.svg"
+  },
+  {
+    "revision": "ad45e1bb7d47f9c3642c781af81cec90",
+    "url": "/librelibrus/img/house.ad45e1bb.svg"
+  },
+  {
+    "revision": "519c498fbd79eb1a84b2199aabdb8626",
+    "url": "/librelibrus/img/mailbox.519c498f.svg"
+  },
+  {
+    "revision": "96657e989068cc2532469f4cb50a70c3",
+    "url": "/librelibrus/img/ravensiris-logo.96657e98.png"
+  },
+  {
+    "revision": "0a8bc57bc313a5eea84cca00c73fc234",
+    "url": "/librelibrus/index.html"
+  },
+  {
+    "revision": "a3673cde36de1467d4c9",
+    "url": "/librelibrus/js/app.ffb264e5.js"
+  },
+  {
+    "revision": "9801edc13feed3f43f3e",
+    "url": "/librelibrus/js/chunk-339dc444.87719823.js"
+  },
+  {
+    "revision": "bbaf2a10486f37bdbb55",
+    "url": "/librelibrus/js/chunk-3f6f6742.8439c004.js"
+  },
+  {
+    "revision": "093782eaa1fd8a94773f",
+    "url": "/librelibrus/js/chunk-5ff8d886.809cfe5f.js"
+  },
+  {
+    "revision": "8fe0d42320c18b433e09",
+    "url": "/librelibrus/js/chunk-9d33c6c4.016ffb01.js"
+  },
+  {
+    "revision": "ab2bd1659f07e306259f",
+    "url": "/librelibrus/js/chunk-vendors.2abdc79e.js"
+  },
+  {
+    "revision": "3f2414ac18657d34b187f39410443263",
+    "url": "/librelibrus/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/librelibrus/robots.txt"
+  }
+]);
