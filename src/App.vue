55572eaa1f99d0fe456c2e50dcad4d749a3d6df8
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-02 02:55:38 +0100

    Setup tailwind + no semicolons

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router"
+import HelloWorld from "@/components/HelloWorld.vue"
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -17,11 +23,13 @@ import HelloWorld from '@/components/HelloWorld.vue'
     </div>
   </header>
 
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+
   <RouterView />
 </template>
 
 <style>
-@import '@/assets/base.css';
+@import "@/assets/base.css";
 
 #app {
   max-width: 1280px;
