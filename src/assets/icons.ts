f0fae7b991fa0dc90e8dd33b3fc9d8edd8cd5f6b
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-03 05:59:51 +0100

    Add icon

--- src/assets/icons.ts
+++ src/assets/icons.ts
@@ -5,6 +5,7 @@ export type IconType =
   | "calendar-outline"
   | "inbox"
   | "inbox-outline"
+  | "clock-time-four-outline"
 
 export type IconMap = {
   [icon_type in IconType]: string
@@ -22,4 +23,6 @@ export const icons: IconMap = {
     "M19,15H15A3,3 0 0,1 12,18A3,3 0 0,1 9,15H5V5H19M19,3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3Z",
   "inbox-outline":
     "M19 3H5A2 2 0 0 0 3 5V19A2 2 0 0 0 5 21H19A2 2 0 0 0 21 19V5A2 2 0 0 0 19 3M5 19V17H8.13A4.13 4.13 0 0 0 9.4 19M19 19H14.6A4.13 4.13 0 0 0 15.87 17H19M19 15H14V16A2 2 0 0 1 10 16V15H5V5H19Z",
+  "clock-time-four-outline":
+    "M12 20C16.4 20 20 16.4 20 12S16.4 4 12 4 4 7.6 4 12 7.6 20 12 20M12 2C17.5 2 22 6.5 22 12S17.5 22 12 22C6.5 22 2 17.5 2 12C2 6.5 6.5 2 12 2M17 13.9L16.3 15.2L11 12.3V7H12.5V11.4L17 13.9Z",
 }
